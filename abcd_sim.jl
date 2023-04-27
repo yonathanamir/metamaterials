d31c2aae467b00049c48a8be073e0a94512c0e56
Yonathan <tunamir18@gmail.com>
2023-04-27 19:18:15 +0300

    Fixes after talk w/ Hadar.

--- abcd_sim.jl
+++ abcd_sim.jl
@@ -1,28 +1,27 @@
 using Plots
 
 
-function gen_γ_Z₀(R, G, C, L, d)
+function gen_tl_γ_Z₀(R, G, C, L, d)
     function _inner(ω)
-        # a₁ = (L₀ - 1/(ω^2*C*d))
-        # a₂ = (C₀ - 1/(ω^2*L*d))
         a₁ = (R+im*ω*L)
         a₂ = (G+im*ω*C)
 
-        kₘ = ω * sqrt(a₁*a₂)
+        γ = sqrt(a₁*a₂)
         Z₀ = sqrt(a₁/a₂)
 
-        return im * kₘ, Z₀
+        return γ, Z₀
     end
 
     return _inner
 end
 
 
-function abcd_transmission_line(L₀, C₀, C, L, d, l)
-    gamma_z_func = gen_γ_Z₀(L₀, C₀, C, L, d)
+function abcd_transmission_line(R, G, C, L, d, l)
+    gamma_z_func = gen_tl_γ_Z₀(R, G, C, L, d)
 
     function _inner(ω)
         γ, Z = gamma_z_func(ω)
+        # println("ω=$ω: γ=$γ, Z=$Z")
 
         return [
             cos(γ*l) im*Z*sin(γ*l);
@@ -87,38 +86,19 @@ function impedence_inductor(L)
 end
 
 
-# function abcd_clrh_cell(L₀, C₀, C, L, d)
-#     tl1 = abcd_transmission_line(L₀, C₀, C, L, d, 3*d/4)
-#     inductor = abcd_shunt_element(impedence_inductor(L))
-#     tl2 = abcd_transmission_line(L₀, C₀, C, L, d, d/4)
-#     capacitator = abcd_series_element(impedence_capacitator(C))
-
-#     function _inner(ω)
-#         tl1_ω = tl1(ω)
-#         inductor_ω = inductor(ω)
-#         tl2_ω = tl2(ω)
-#         capacitator_ω = capacitator(ω)
-
-#         return tl1_ω * inductor_ω * tl2_ω * capacitator_ω
-#     end
-
-#     return _inner
-# end
-
-
 function abcd_clrh_cell(L₀, C₀, C, L, d)
-    inductor = abcd_series_element(impedence_inductor(L₀*d))
-    inductor_ground = abcd_shunt_element(impedence_inductor(L*d))
-    capacitator = abcd_series_element(impedence_capacitator(C*d))
-    capacitator_ground = abcd_shunt_element(impedence_capacitator(C₀*d))
+    tl1 = abcd_transmission_line(0, 0, C₀, L₀, d, 3*d/4)
+    inductor = abcd_shunt_element(impedence_inductor(L))
+    tl2 = abcd_transmission_line(0, 0, C₀, L₀, d, d/4)
+    capacitator = abcd_series_element(impedence_capacitator(C))
 
     function _inner(ω)
-        is = inductor(ω)
-        ig = inductor_ground(ω)
-        cs = capacitator(ω)
-        cg = capacitator_ground(ω)
+        tl1_ω = tl1(ω)
+        inductor_ω = inductor(ω)
+        tl2_ω = tl2(ω)
+        capacitator_ω = capacitator(ω)
 
-        return is * cs * ig * cg
+        return tl1_ω * inductor_ω * tl2_ω * capacitator_ω
     end
 
     return _inner
@@ -127,10 +107,14 @@ end
 
 function abcd_metamaterial_system(L₀, C₀, C, L, d, num_cells)
     function _inner(ω)
-        sys = ones(2, 2)
+        sys = [1 0; 0 1]
+
         for _ in collect(1:num_cells)
             sys *= abcd_clrh_cell(L₀, C₀, C, L, d)(ω)
         end
+
+        sys *= abcd_series_element(impedence_resistor(50))(ω)
+
         return sys
     end
 
@@ -141,35 +125,35 @@ end
 function abcd_sim(ωs, abcd_system)
     end_vals = [1, 1/50]
 
-    # unpack(x) = (x[1] + 50*x[2])/(x[1] - 50*x[2])
-
     return [
-        unpack(real(abcd_system(ω) * end_vals)) for ω in ωs
+        unpack((abcd_system(ω) * end_vals)) for ω in ωs
     ]
 end
 
 
 function unpack(x)
-    return (x[1] + 50*x[2])/(x[1] - 50*x[2])
+    return real((x[1] + 50*x[2])/(x[1] - 50*x[2]))
 end
 
 
-L₀ = 7.9
-C₀ = 84.5
+# L₀ = 1.01e-7
+# C₀ = 2.83e-11
+
+L₀ = 1.58-7
+C₀ = 1.81e-11
+
 L = 5.6e-9
 C = 1e-12
-d = 1e-3
+d = 1e-2
 
-# ωs = collect(1e8: 1e6: 10e9)
-ω = 3e9
+ωs = collect(500e6: 1e7: 14e9) * 2*π
+abcd_system = abcd_metamaterial_system(L₀, C₀, C, L, d, 10)
+sim_results = abcd_sim(ωs, abcd_system)
+plot(ωs/(2*π), sim_results)
 
-abcd_system = abcd_metamaterial_system(L₀, C₀, C, L, d, 9)(ω)
-
-end_vals = [10e100, 10e100/50]
-init_vals = abcd_system * end_vals
-
-res = unpack(init_vals)
-
-# sim_results = abcd_sim(ωs, abcd_system)
-
-# scatter(ωs, round.(sim_results*1000000))
+# ω = 3e9 * 2*π
+# abcd_system = abcd_metamaterial_system(L₀, C₀, C, L, d, 1)(ω)
+# end_vals = [1, 1/50]
+# init_vals = abcd_system * end_vals
+# res = unpack(init_vals)
+# print(res)
